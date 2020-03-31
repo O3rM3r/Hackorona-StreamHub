4787d22694504802016592b96b0264ff7fc7a910
Tal Maital <tal@mustry.co>
2020-03-31 00:43:41 -0700

    commit

---
+++ livey-client/src/Components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-days-container">
+        
+      </div>
+      <div className="app-categories-container">
+
+      </div>
+      <div className="app-feed-container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
