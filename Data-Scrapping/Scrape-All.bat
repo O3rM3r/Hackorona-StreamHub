604d83b0e6ed4b4bca228cacfa6a2ff0910657e0
O3rM3r <omer.mayer@gmail.com>
2020-04-04 14:12:54 +0300

    scrape batch

---
+++ Data-Scrapping/Scrape-All.bat
@@ -0,0 +1,5 @@
+@echo off
+python Meetup-Scrape.py
+python Zappa-Scrape.py
+python Baavir-Scrape.py
+python Eventbrite-Scrape.py
---
+++ Data-Scrapping/Scrape-All.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python Eventbrite-Scrape.py
+python Meetup-Scrape.py
+python Zappa-Scrape.py
+python Baavir-Scrape.py
